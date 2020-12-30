5458a0af895a246b629fda092c532ff0ee57c97d
Roger Yu-Hsiang Lo <b13472003@gmail.com>
2020-12-30 12:31:18 -0800

    Implemented check_unsaved_corpus and check_duplicated_gloss in decorator.py

--- src/main/python/gui/decorator.py
+++ src/main/python/gui/decorator.py
@@ -1,7 +1,9 @@
+import os
 import functools
 from datetime import date
 from PyQt5.QtWidgets import (
-    QMessageBox
+    QMessageBox,
+    QFileDialog
 )
 
 
@@ -43,3 +45,42 @@ def check_empty_gloss(func):
         else:
             return func(self, *args, **kwargs)
     return wrapper_check_empty_gloss
+
+
+def check_duplicated_gloss(func):
+    @functools.wraps(func)
+    def wrapper_duplicated_gloss(self, *args, **kwargs):
+        lexical_info = self.lexical_scroll.get_value()
+        if lexical_info is None:
+            return
+        else:
+            if self.current_sign:
+                if lexical_info['gloss'] == self.current_sign.lexical_information.gloss or lexical_info['gloss'] not in self.corpus.get_sign_glosses():
+                    return func(self, *args, **kwargs)
+
+            if lexical_info['gloss'] in self.corpus.get_sign_glosses():
+                QMessageBox.critical(self, 'Duplicated Gloss',
+                                     'Please use a different gloss. Duplicated glosses are not allowed.')
+                return
+            else:
+                return func(self, *args, **kwargs)
+    return wrapper_duplicated_gloss
+
+
+def check_unsaved_corpus(func):
+    @functools.wraps(func)
+    def wrapper_check_unsaved_corpus(self, *args, **kwargs):
+        if self.corpus.path is None:
+            file_name, _ = QFileDialog.getSaveFileName(self,
+                                                       self.tr('Save Corpus'),
+                                                       os.path.join(self.app_settings['storage']['recent_folder'],
+                                                                    'corpus.slpaa'),
+                                                       self.tr('SLAP-AA Corpus (*.slpaa)'))
+            if file_name:
+                self.corpus.path = file_name
+                folder, _ = os.path.split(file_name)
+                if folder:
+                    self.app_settings['storage']['recent_folder'] = folder
+        return func(self, *args, **kwargs)
+
+    return wrapper_check_unsaved_corpus
