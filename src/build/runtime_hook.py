f8dce54abcd2d681938a112f416117d5218f3627
stannam <stan.nam98@gmail.com>
2022-09-30 12:47:32 -0700

    trying to make an executable

---
+++ src/build/runtime_hook.py
@@ -0,0 +1,19 @@
+import importlib
+import json
+import sys
+from os import path
+meipass_path = sys._MEIPASS
+
+# debugging
+print(path.dirname(sys.executable))
+input("break")
+
+# end debugging
+
+with open(path.join(meipass_path, 'runtime', 'base.json')) as f:
+    settings = json.load(f)
+
+module = importlib.import_module('fbs_runtime._frozen')
+module.BUILD_SETTINGS = settings
+module.BUILD_SETTINGS['environment'] = 'local'
+module.BUILD_SETTINGS['meipass_path'] = meipass_path
