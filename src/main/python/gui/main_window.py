8801c4581e5761203f0d2328e13e7c41d4cdb8dc
Grace Zhang <gracemyzhang@gmail.com>
2023-05-17 18:13:22 -0700

    Clear sign summary and sign level panels when loading blank corpus

--- src/main/python/gui/main_window.py
+++ src/main/python/gui/main_window.py
@@ -884,6 +884,12 @@ class MainWindow(QMainWindow):
         self.corpus_display.updated_signs(self.corpus.signs)
         if len(self.corpus.signs) > 0:
             self.corpus_display.selected_sign.emit((list(self.corpus.signs))[0])
+        else: # if loading a blank corpus
+            self.signsummary_panel.mainwindow.current_sign = None # refreshsign() checks for this
+            self.signsummary_panel.refreshsign(None)
+            self.signlevel_panel.clear()
+            self.signlevel_panel.enable_module_buttons(False)
+            
 
         return self.corpus is not None  # bool(Corpus)
 
