3c41dec457913d1e262928de5e13809305398c46
kvesik <33950744+kvesik@users.noreply.github.com>
2024-05-03 19:48:05 -0700

    added maximal vs minimal information options for corpus export
    also ensured that status updates for both export and merge corpora display in real time

--- src/main/python/gui/exportcorpus_dialog.py
+++ src/main/python/gui/exportcorpus_dialog.py
@@ -14,6 +14,7 @@ from PyQt5.QtWidgets import (
 )
 
 from gui.modulespecification_widgets import StatusDisplay
+from gui.helper_widget import OptionSwitch
 
 
 class ExportCorpusDialog(QDialog):
@@ -23,17 +24,23 @@ class ExportCorpusDialog(QDialog):
         self.app_settings = app_settings
         self.exportfilepath = ""
         self.outputformat = "json"
+        self.detaillevel = "max"
 
         main_layout = QVBoxLayout()
         form_layout = QFormLayout()
 
-        self.selectformatlabel = QLabel("1. Choose which format you'd like for the exported data: JSON (.txt) is currently the only option.")
+        self.selectformatlabel = QLabel("1a. Choose which format you'd like for the exported data: JSON (.txt) is currently the only option.")
         self.selectformatcombo = QComboBox()
         self.selectformatcombo.addItems(["JSON (.txt)"])
         self.selectformatcombo.currentTextChanged.connect(self.formatcombo_changed)
         form_layout.addRow(self.selectformatlabel, self.selectformatcombo)
+        self.selectdetaillabel = QLabel("1b. Choose whether you'd like maximal information (all attribute values, even if they're empty/false/0) or minimal (only specified values).")
+        self.selectdetailswitch = OptionSwitch("Maximal", "Minimal")
+        self.selectdetailswitch.toggled.connect(self.handle_detailswitch_toggled)
+        form_layout.addRow(self.selectdetaillabel, self.selectdetailswitch)
         self.chooseexportedfilelabel = QLabel("2. Choose the name and location for the exported file.")
         self.chooseexportedfilebutton = QPushButton("Select exported file")
+        self.chooseexportedfilebutton.setEnabled(False)
         self.chooseexportedfilebutton.clicked.connect(self.handle_select_exportedfile)
         form_layout.addRow(self.chooseexportedfilelabel, self.chooseexportedfilebutton)
         self.exportcorpuslabel = QLabel("3. Export current .slpaa file.")
@@ -59,6 +66,14 @@ class ExportCorpusDialog(QDialog):
 
         self.setLayout(main_layout)
 
+    def handle_detailswitch_toggled(self, selection_dict):
+        if selection_dict[1]:
+            self.detaillevel = "max"
+        else:
+            self.detaillevel = "min"
+        self.statusdisplay.setText(self.detaillevel + "imal detail selected")
+        self.chooseexportedfilebutton.setEnabled(True)
+
     def formatcombo_changed(self, txt):
         if "json" in txt.lower():
             newoutputformat = "json"
@@ -82,16 +97,26 @@ class ExportCorpusDialog(QDialog):
 
     def handle_export_corpus(self):
         self.statusdisplay.setText("exporting...")
+        self.statusdisplay.repaint()
         resultmessage = self.export_corpus()
         self.statusdisplay.setText(resultmessage)
 
     def export_corpus(self):
         serialized_corpus = self.parent().corpus.serialize()
         with io.open(self.exportfilepath, "w") as exfile:
-            # can also specify separators=(item_separator, key_separator)
-            # default is (', ', ': ') if indent is None and (',', ': ') otherwise
             try:
-                json.dump(serialized_corpus, exfile, indent=3, default=lambda x: getattr(x, '__dict__', str(x)))
+                # OK, this is a bit convoluted, but it seems like the most general way to be able to omit values
+                # that are empty/zero/false/null, is to convert everything to json format and read it back in so
+                # all of the data is in either dicts or lists (rather than objects).
+                # If we end up wanting to do something prettier or more customized in the future,
+                # this kind of cleaning might have to be done in the data classes themselves.
+
+                # for json.dumps, can also specify separators=(item_separator, key_separator)
+                # default is (', ', ': ') if indent is None and (',', ': ') otherwise
+                thestring = json.dumps(serialized_corpus, indent=3, default=lambda x: getattr(x, '__dict__', str(x)))
+                reloaded = json.loads(thestring)
+                cleaned = cleandictsforexport(reloaded, self.detaillevel)
+                json.dump(cleaned, exfile, indent=3, default=lambda x: getattr(x, '__dict__', str(x)))
             except Exception:
                 return "export failed"
 
@@ -103,3 +128,41 @@ class ExportCorpusDialog(QDialog):
         if standard == QDialogButtonBox.Close:
             # close dialog
             self.accept()
+
+
+def cleandictsforexport(serialstructure, detaillevel):
+    if detaillevel == "max":
+        return serialstructure
+    elif detaillevel == "min":
+        if isinstance(serialstructure, dict):
+            cleaned_dict = {}
+            for k, v in serialstructure.items():
+                if k in ["timingintervals", "_timingintervals"]:
+                    # do not omit any info from these items; it makes them hard to read
+                    cleaned_dict[k] = v
+                elif k == "col_labels":
+                    # don't worry about including the column labels for the location details;
+                    # it should be clear from the contents what we're looking at
+                    pass
+                else:
+                    cleaned_item = cleandictsforexport(v, detaillevel)
+                    if cleaned_item:
+                        cleaned_dict[k] = cleaned_item
+            return cleaned_dict
+        elif isinstance(serialstructure, list):
+            cleaned_list = []
+            if len(serialstructure) == 2 and isinstance(serialstructure[0], str) and isinstance(serialstructure[1], bool) and not serialstructure[1]:
+                # it's a key-value pair of some sort; if the second element is false we don't want the first one either
+                pass
+            else:
+                for v in serialstructure:
+                    cleaned_item = cleandictsforexport(v, detaillevel)
+                    if cleaned_item:
+                        cleaned_list.append(cleaned_item)
+            return cleaned_list
+        elif isinstance(serialstructure, str) or isinstance(serialstructure, float) or isinstance(serialstructure, int) or isinstance(serialstructure, bool) or serialstructure is None:
+            if serialstructure and serialstructure != 0:
+                # if it's a pseudo-primite data type (even though yes, I know, that's not a thing for Python)
+                # then just return it as is, as long as it's not empty/zero/false
+                return serialstructure
+            return None
