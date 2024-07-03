f7e76c4fd00e224a65cf129b020cc5a91fa9a252
kvesik <33950744+kvesik@users.noreply.github.com>
2024-07-03 15:59:27 -0700

    add include tongue, upper teeth, and lower teeth placeholders to the .svg file regions

--- src/main/resources/base/predefined_locations/add_regions_to_svg_files.py
+++ src/main/resources/base/predefined_locations/add_regions_to_svg_files.py
@@ -3,19 +3,29 @@ import os
 import shutil
 
 
+# adds regions for placeholder images: selected fingers (& thumb), tongue, upper/lower teeth
 def main():
-    # for destfolder in ["green_HL_withregions", "yellow_HL_withregions", "violet_HL_withregions"]:
-    #     srcfolder = destfolder.replace("_withregions", " - original (no regions)")
-    for srcfolder in ["yellow_20240508version", "green_20240508version", "violet_20240508version"]:
-        destfolder = srcfolder + "_withselectedregions"
+    for colour in ["yellow", "green", "violet"]:
+        srcfolder = colour + "_20240508version_noselectedfingers"
+        destfolder = colour + "_20240703version"
         if not os.path.exists(destfolder):
             os.mkdir(destfolder)
         srcfilenames_frontview = [fname for fname in os.listdir(srcfolder) if not isbackview(fname)]
         srcfilenames_backview = [fname for fname in os.listdir(srcfolder) if isbackview(fname)]
-        # replace_20240508("front", srcfilenames_frontview, srcfolder, destfolder)
-        # replace_20240508("back", srcfilenames_backview, srcfolder, destfolder)
         replace_20240703("front", srcfilenames_frontview, srcfolder, destfolder)
         replace_20240703("back", srcfilenames_backview, srcfolder, destfolder)
+
+
+# adds (invisible) regions for all locations
+def main_20240508():
+    for destfolder in ["green_HL_withregions", "yellow_HL_withregions", "violet_HL_withregions"]:
+        srcfolder = destfolder.replace("_withregions", " - original (no regions)")
+        if not os.path.exists(destfolder):
+            os.mkdir(destfolder)
+        srcfilenames_frontview = [fname for fname in os.listdir(srcfolder) if not isbackview(fname)]
+        srcfilenames_backview = [fname for fname in os.listdir(srcfolder) if isbackview(fname)]
+        replace_20240508("front", srcfilenames_frontview, srcfolder, destfolder)
+        replace_20240508("back", srcfilenames_backview, srcfolder, destfolder)
 
 
 def replace_20240703(frontorback, srcfilenames, srcfolder, destfolder):
@@ -32,11 +42,17 @@ def replace_20240703(frontorback, srcfilenames, srcfolder, destfolder):
                     ln = src.readline()
                     while len(ln) > 0:
                         if '<g id="Right_Fingers_and_thumb" data-name="Right Fingers and thumb">' in ln:
-                            with io.open("selectedfingersandthumb_replacement_" + frontorback + ".txt", "r") as sft_replace:
+                            with io.open("selectedfingersandthumb_replacement_front.txt", "r") as sft_replace:
                                 dst.writelines(sft_replace.readlines())
                         elif '<g id="Right_Fingers" data-name="Right Fingers">' in ln:
-                            with io.open("selectedfingers_replacement_" + frontorback + ".txt", "r") as sf_replace:
+                            with io.open("selectedfingers_replacement_front.txt", "r") as sf_replace:
                                 dst.writelines(sf_replace.readlines())
+                        elif '<g id="Mouth" data-name="Mouth">' in ln:
+                            with io.open("tongue_replacement_front.txt", "r") as tongue_replace:
+                                dst.writelines(tongue_replace.readlines())
+                        elif '<g id="Teeth" data-name="Teeth">' in ln:
+                            with io.open("upperlowerteeth_replacement_front.txt", "r") as ulteeth_replace:
+                                dst.writelines(ulteeth_replace.readlines())
                         else:
                             dst.write(ln)
                         ln = src.readline()
